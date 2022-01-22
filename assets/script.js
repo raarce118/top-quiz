5baf0ecbaacabb0c1eba1c47aac4d33d07571c93
Rich Arce <raarce118@gmail.com>
2022-01-22 16:02:58 -0700

    added java script for button and lay out of quiz

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var quizQuestions = document.getElementById("quiz-questions");
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+var timecounter = document.getElementById("timecounter");
+var titleitem = document.getElementById("title-item");
+var nextQuestions 
+var questionanswers = document.getElementById("question-answers");
+var myScore = document.getElementById("score");
+var btnScore = document.getElementById("btnScore");
+var currentindex = 0;
+var score = 0;
+var count = 60;
+var alert =document.getElementById("alert");
+var info = document.getElementById("info");
+
+var allScores = [];
+var questions =
