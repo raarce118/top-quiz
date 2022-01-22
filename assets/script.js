085a193aceb42e3c6bc83892e0fdbc0b5526f976
Rich Arce <raarce118@gmail.com>
2022-01-22 16:42:42 -0700

    added questions to java script

--- assets/script.js
+++ assets/script.js
@@ -14,4 +14,40 @@ var alert =document.getElementById("alert");
 var info = document.getElementById("info");
 
 var allScores = [];
-var questions =
+
+var questions = [
+    {
+        title: "Commonly used datypes DO NOT include:---",
+        choices: ["strings","booleans","alerts", "numbers"],
+        answer : "alerts"    
+    },
+
+    {
+        title: "The condition in an if/else statement is enclosed within:---",
+        choices: ["quotes","Curly brackets","parentheses", "square brackets"],
+        answer : "parentheses"
+        
+    },
+
+    {  title: "Arrays in Javascript can be used to store _______:---",
+       choices: ["numbers and strings", "other Arrays", "booleans", "all of the above"],
+       answer: "all of the above"
+
+
+    },
+
+    {  title: "String values must be enclosed within _____ when being assigned to variables:---",
+       choices: ["commas", "curley brackets", "quotes", "parenthesis"],
+       answer: "quotes"
+
+
+    },
+
+    {  title: "A very usefuly tool used during development and debugging for printing content to the debugger is:---",
+    choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
+
+     },
+
+ 
+  
