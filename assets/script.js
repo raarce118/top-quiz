0a28542470c4b1dcb69065ca4cf7cf1816883cf8
Rich Arce <raarce118@gmail.com>
2022-01-25 11:22:25 -0700

    fixed next question issue

--- assets/script.js
+++ assets/script.js
@@ -9,45 +9,150 @@ var myScore = document.getElementById("score");
 var btnScore = document.getElementById("btnScore");
 var currentindex = 0;
 var score = 0;
-var count = 60;
+var count = 75;
 var alert =document.getElementById("alert");
 var info = document.getElementById("info");
-
 var allScores = [];
-
+var storedScores = JSON.parse(localStorage.getItem("userData"));
 var questions = [
-    {
-        title: "Commonly used datypes DO NOT include:---",
+
+     {
+        title: "Commonly used datypes DO NOT include:",
         choices: ["strings","booleans","alerts", "numbers"],
         answer : "alerts"    
     },
 
     {
-        title: "The condition in an if/else statement is enclosed within:---",
+        title: "The condition in an if/else statement is enclosed within:",
         choices: ["quotes","Curly brackets","parentheses", "square brackets"],
         answer : "parentheses"
         
     },
 
-    {  title: "Arrays in Javascript can be used to store _______:---",
+    {  title: "Arrays in Javascript can be used to store _______:",
        choices: ["numbers and strings", "other Arrays", "booleans", "all of the above"],
        answer: "all of the above"
 
 
     },
 
-    {  title: "String values must be enclosed within _____ when being assigned to variables:---",
+    {  title: "String values must be enclosed within _____ when being assigned to variables:",
        choices: ["commas", "curley brackets", "quotes", "parenthesis"],
        answer: "quotes"
 
 
     },
 
-    {  title: "A very usefuly tool used during development and debugging for printing content to the debugger is:---",
+    {  title: "A very usefuly tool used during development and debugging for printing content to the debugger is:",
     choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
     answer: "console.log"
 
-     },
+     }
 
- 
-  
+]
+
+btnStart.addEventListener("click", startQuiz);
+function startQuiz(){
+    if(storedScores !==null) {
+        allScores = storedScores;
+    }
+    info.classList.add("d-none")
+    btnStart.classList.add("d-none")
+    timecounter.classList.remove("d-none")
+    quizQuestions.classList.remove("d-none")
+    nextQuestions = questions[currentindex]
+    console.log(nextQuestions.title)
+    
+        displayQuestion(nextQuestions)
+
+    gametime()
+}
+btnScore.addEventListener("click" , function(){
+    let name = document.getElementById("inputScore").value
+    scorePage(name, count)
+});
+
+function gametime(){
+
+    var timeinterval = setInterval(function(){
+        timer.innerText = count
+         count--;
+        }, 1000);
+
+}
+
+function scorePage(a, b) {
+
+    var userData = {
+        inits: a,
+        userScore: b
+    };
+    allScores.push(userData);
+
+    localStorage.setItem("userData", JSON.stringify(allScores));
+
+}
+
+
+function displayQuestion(question){
+    titleitem.innerText=question.title
+    question.choices.forEach(element => {
+     var button =document.createElement("button")
+    button.className="btn-primary btn-block text-left"
+    button.innerText=element
+    questionanswers.appendChild(button)
+    button.addEventListener("click", displaynextQuestion)
+    });
+}
+
+
+function displaynextQuestion(e){
+    currentindex++
+    if(currentindex < questions.length){
+        correction(e.target.innerText == nextQuestions.answer)
+        questionanswers.innerHTML=""
+        if(currentindex < questions.length){    
+            nextQuestions= questions[currentindex]
+            displayQuestion(nextQuestions)  
+        }else {
+            currentindex = 0
+            displayQuestion(nextQuestions)  
+        }
+
+    }else{
+        console.log("endgame")
+        endgame()
+        
+
+    }
+    
+     
+}
+function correction(response){
+    
+    if(response){
+        alert.innerText= "Good"
+        console.log("Good")
+    }else {
+        alert.innerText="Wrong"
+        count = count -15
+        timer.innerHTML = count
+        console.log("Wrong")
+
+    }
+    setTimeout(function(){
+        alert.innerText=""
+    
+        }, 1000);
+
+}
+ function endgame (){
+    // btnStart.classList.add("d-none")
+    myScore.innaText = count
+    addscore.classList.remove("d-none")
+    timecounter.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+    addscore.classList.remove("d-none")
+
+
+ }
